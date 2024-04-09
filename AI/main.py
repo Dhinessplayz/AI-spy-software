f733087b37c1677eae923509f05b0b5ddd0167ff
Dhiness Karthikeyan <dhinesskarthick@gmail.com>
2024-04-09 17:10:32 +0100

    Base for the AI part of the code
    A simple base has been created for the AI part of this project. There isn't really any AI implemented yet and the Keylogger still has to be linked for further process

---
+++ AI/main.py
@@ -0,0 +1,25 @@
+import time
+import sys
+from Keylogger #Import keylogger after adding it
+
+
+start_input = input("")
+
+
+
+while True:
+    def intro():
+        print("Hello, my name is E$do, your personal AI trained to help make your Spyware information easy to access")
+        time.sleep(0.9)
+        print("Disclaimer: I am only allowed to provide information on data that I have permission to access")
+        time.sleep(0.9)
+        print("Don't misuse the data you have access to and make sure to get consent before using this on someone!")
+    def input_def():
+
+        start_input = input("hello") #Most likely will be the base input source
+
+    if start_input != "":
+        def intro()
+
+    else:
+        input_def()
